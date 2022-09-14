e4f169036eff2cd56747a874d1fc57be2ba75201
Rams1n <113514507+Rams1n@users.noreply.github.com>
2022-09-14 18:18:15 +0800

    Add files via upload

---
+++ Form1.vb
@@ -0,0 +1,130 @@
+﻿Public Class Form1
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+
+    End Sub
+
+    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Label2.Text = Val(TextBox1.Text) + Val(TextBox2.Text)
+        Label1.Text = "+"
+        If Label2.Text = "69" Then Label2.Text = "nice"
+        If Label2.Text = "420" Then Label2.Text = "Weed"
+        If Label2.Text = "360" Then Label2.Text = "full circle"
+        If Label2.Text = "39" Then Label2.Text = "virgin"
+        If Label2.Text = "19" Then Label2.Text = "fortnite card"
+        If Label2.Text = "90001" Then Label2.Text = "broken scanner*"
+        If Label2.Text = "5433345" Then Label2.Text = "SHEEESH"
+        If Label2.Text = "54333345" Then Label2.Text = "SHEEEESH"
+        If Label2.Text = "54333345" Then Label2.Text = "SHEEEEESH"
+        If Label2.Text = "543345" Then Label2.Text = "SHEESH"
+        If Label2.Text = "0" Then Label2.Text = "how?"
+        If Label2.Text = "1" Then Label2.Text = "why?"
+        If Label2.Text = "14" Then Label2.Text = "hi"
+        If Label2.Text = "80085" Then Label2.Text = "bobs"
+        If Label2.Text = "8008135" Then Label2.Text = "bobies"
+        If Label2.Text = "403" Then Label2.Text = "hoe"
+        If Label2.Text = "7734" Then Label2.Text = "hell"
+        If Label2.Text = "90" Then Label2.Text = "right"
+        If Label2.Text = "91" Then Label2.Text = "left"
+        If Label2.Text = "89" Then Label2.Text = "acute"
+        If Label2.Text = "99" Then Label2.Text = "gg"
+        If Label2.Text = "98" Then Label2.Text = "nt"
+        If Label2.Text = "5" Then Label2.Text = "ace"
+        If Label2.Text = "8" Then Label2.Text = "afro kobe"
+        If Label2.Text = "24" Then Label2.Text = "kobe"
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Label2.Text = Val(TextBox1.Text) - Val(TextBox2.Text)
+        Label1.Text = "-"
+        If Label2.Text = "69" Then Label2.Text = "nice"
+        If Label2.Text = "420" Then Label2.Text = "Weed"
+        If Label2.Text = "360" Then Label2.Text = "full circle"
+        If Label2.Text = "39" Then Label2.Text = "virgin"
+        If Label2.Text = "19" Then Label2.Text = "fortnite card"
+        If Label2.Text = "90001" Then Label2.Text = "broken scanner*"
+        If Label2.Text = "5433345" Then Label2.Text = "SHEEESH"
+        If Label2.Text = "54333345" Then Label2.Text = "SHEEEESH"
+        If Label2.Text = "54333345" Then Label2.Text = "SHEEEEESH"
+        If Label2.Text = "543345" Then Label2.Text = "SHEESH"
+        If Label2.Text = "0" Then Label2.Text = "how?"
+        If Label2.Text = "1" Then Label2.Text = "why?"
+        If Label2.Text = "14" Then Label2.Text = "hi"
+        If Label2.Text = "80085" Then Label2.Text = "bobs"
+        If Label2.Text = "8008135" Then Label2.Text = "bobies"
+        If Label2.Text = "403" Then Label2.Text = "hoe"
+        If Label2.Text = "7734" Then Label2.Text = "hell"
+        If Label2.Text = "90" Then Label2.Text = "right"
+        If Label2.Text = "91" Then Label2.Text = "left"
+        If Label2.Text = "89" Then Label2.Text = "acute"
+        If Label2.Text = "99" Then Label2.Text = "gg"
+        If Label2.Text = "98" Then Label2.Text = "nt"
+        If Label2.Text = "5" Then Label2.Text = "ace"
+        If Label2.Text = "8" Then Label2.Text = "afro kobe"
+        If Label2.Text = "24" Then Label2.Text = "kobe"
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Label2.Text = Val(TextBox1.Text) * Val(TextBox2.Text)
+        Label1.Text = "*"
+        If Label2.Text = "69" Then Label2.Text = "nice"
+        If Label2.Text = "420" Then Label2.Text = "Weed"
+        If Label2.Text = "360" Then Label2.Text = "full circle"
+        If Label2.Text = "39" Then Label2.Text = "virgin"
+        If Label2.Text = "19" Then Label2.Text = "fortnite card"
+        If Label2.Text = "90001" Then Label2.Text = "broken scanner*"
+        If Label2.Text = "5433345" Then Label2.Text = "SHEEESH"
+        If Label2.Text = "54333345" Then Label2.Text = "SHEEEESH"
+        If Label2.Text = "54333345" Then Label2.Text = "SHEEEEESH"
+        If Label2.Text = "543345" Then Label2.Text = "SHEESH"
+        If Label2.Text = "0" Then Label2.Text = "how?"
+        If Label2.Text = "1" Then Label2.Text = "why?"
+        If Label2.Text = "14" Then Label2.Text = "hi"
+        If Label2.Text = "80085" Then Label2.Text = "bobs"
+        If Label2.Text = "8008135" Then Label2.Text = "bobies"
+        If Label2.Text = "403" Then Label2.Text = "hoe"
+        If Label2.Text = "7734" Then Label2.Text = "hell"
+        If Label2.Text = "90" Then Label2.Text = "right"
+        If Label2.Text = "91" Then Label2.Text = "left"
+        If Label2.Text = "89" Then Label2.Text = "acute"
+        If Label2.Text = "99" Then Label2.Text = "gg"
+        If Label2.Text = "98" Then Label2.Text = "nt"
+        If Label2.Text = "5" Then Label2.Text = "ace"
+        If Label2.Text = "8" Then Label2.Text = "afro kobe"
+        If Label2.Text = "24" Then Label2.Text = "kobe"
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Label2.Text = Val(TextBox1.Text) / Val(TextBox2.Text)
+        Label1.Text = "/"
+        If Label2.Text = "69" Then Label2.Text = "nice"
+        If Label2.Text = "420" Then Label2.Text = "Weed"
+        If Label2.Text = "360" Then Label2.Text = "full circle"
+        If Label2.Text = "39" Then Label2.Text = "virgin"
+        If Label2.Text = "19" Then Label2.Text = "fortnite card"
+        If Label2.Text = "90001" Then Label2.Text = "broken scanner*"
+        If Label2.Text = "5433345" Then Label2.Text = "SHEEESH"
+        If Label2.Text = "54333345" Then Label2.Text = "SHEEEESH"
+        If Label2.Text = "54333345" Then Label2.Text = "SHEEEEESH"
+        If Label2.Text = "543345" Then Label2.Text = "SHEESH"
+        If Label2.Text = "0" Then Label2.Text = "how?"
+        If Label2.Text = "1" Then Label2.Text = "why?"
+        If Label2.Text = "14" Then Label2.Text = "hi"
+        If Label2.Text = "80085" Then Label2.Text = "bobs"
+        If Label2.Text = "8008135" Then Label2.Text = "bobies"
+        If Label2.Text = "403" Then Label2.Text = "hoe"
+        If Label2.Text = "7734" Then Label2.Text = "hell"
+        If Label2.Text = "90" Then Label2.Text = "right"
+        If Label2.Text = "91" Then Label2.Text = "left"
+        If Label2.Text = "89" Then Label2.Text = "acute"
+        If Label2.Text = "99" Then Label2.Text = "gg"
+        If Label2.Text = "98" Then Label2.Text = "nt"
+        If Label2.Text = "5" Then Label2.Text = "ace"
+        If Label2.Text = "8" Then Label2.Text = "afro kobe"
+        If Label2.Text = "24" Then Label2.Text = "kobe"
+    End Sub
+End Class
